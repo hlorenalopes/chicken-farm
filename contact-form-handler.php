e7be03fec3e26e6fc952ddc572533a53a2b30ed1
Lorena Lopes <hlorenalopes@gmail.com>
2021-05-27 10:05:02 -0300

    fix: remove phone from contact form

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -2,14 +2,12 @@
     $errors = '';
     $myemail = 'hloriwebsite@gmail.com';//<-----Put Your email address here.
     if(empty($_POST['name'])  ||
-       empty($_POST['phone'])  ||
        empty($_POST['email']) ||
        empty($_POST['message']))
     {
         $errors .= "\n Error: all fields are required";
     }
     $name = $_POST['name'];
-    $phone = $_POST['phone'];
     $email_address = $_POST['email'];
     $message = $_POST['message'];
     if (!preg_match(
@@ -25,7 +23,7 @@
     $email_subject = "Contact form submission: $name";
     $email_body = "You have received a new message. ".
     " Here are the details:\n Name: $name \n ".
-    "Phone: $phone\n Email: $email_address\n Message \n $message";
+    "Email: $email_address\n Message \n $message";
     $headers = "From: $myemail\n";
     $headers .= "Reply-To: $email_address";
     mail($to,$email_subject,$email_body,$headers);
